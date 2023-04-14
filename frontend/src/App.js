ceb94d411618ec29836c15bf7e600131bfcae7fa
HoangEevee <nguyenquochoang2806@gmail.com>
2023-04-14 14:42:48 +1000

    temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-
+import { myContext } from "./pages/Context";
+import { useContext } from "react";
 function App() {
+    //This thing has the user data when they logged in
+    const context = useContext(myContext)
     return (
         <BrowserRouter>
             <Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
+import Context from './pages/Context';
 
 axios.defaults.baseURL = "http://localhost:8080/";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>
 );
