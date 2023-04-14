52344edb7d5010c4e03818f366066b116248346d
HoangEevee <nguyenquochoang2806@gmail.com>
2023-04-14 12:20:06 +1000

    forgot about base URL for axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+axios.defaults.baseURL = "http://localhost:8080/";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
